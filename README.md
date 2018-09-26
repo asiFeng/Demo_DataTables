f76735b5e452ac3e4e5f93f7ed49904bdeca2211
ciweihuabai <634754410@qq.com>
2018-09-27 00:01:00 +0800

    Signed-off-by: ciweihuabai <634754410@qq.com> Demo01: 实现最简单的 dataTables 应用

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Demo_DataTables
+### [页面](./index.html)
+
+### Demo01
+> 执行DataTable构造函数，具有设置页面显示个数，检索，分页，排序等功能。
---
+++ demo01.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $("#table_demo01").DataTable({
+        sorting:true
+    });
+});
